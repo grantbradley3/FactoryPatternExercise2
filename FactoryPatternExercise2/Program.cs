94525e6006428a67737911d1f1979d803b8465b3
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-28 18:35:38 -0500

    Initial Commit

--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string userInput;
+            bool correctInput;
+
+            do
+            {
+                correctInput= true;
+                Console.WriteLine("What database would you like to use?");
+                Console.WriteLine("Type: sql");
+                Console.WriteLine("Type: mongo");
+                Console.WriteLine("Type: list");
+
+                userInput = Console.ReadLine();
+
+                if (userInput != "sql" && userInput != "mongo" && userInput != "list")
+                {
+                    correctInput= false;
+                    Console.WriteLine("Incorrect Input");
+                }
+
+            } while (correctInput == false);
+
+
+            IDataAccess db = DataAccessFactory.GetDataAccessType(userInput);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach(var item in products ) 
+            {
+                Console.WriteLine($"Name: {item.Name} Price:{item.Price}");
+            }
+
         }
     }
 }
---
+++ FactoryPatternExercise2/SQLDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product(){Name="Xbox", Price = 350},
+            new Product(){Name="Toothbrush", Price = 50},
+            new Product(){Name="Waterfilter", Price = 100},
+            new Product(){Name="Computer", Price = 1000}
+
+        };
+
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from Mongo Data Access.");
+
+            return Products;    
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a Mongo Data Access");
+        }
+    }
+}
